a04b267e23ca9f4333b3af96644ce3ac552c0c82
Aleksa Majkic <aleksamcode@gmail.com>
2020-10-16 00:23:10 +0200

    Update z02 - 20201007

--- ispit_20201007/z02/script-otisci.sh
+++ ispit_20201007/z02/script-otisci.sh
@@ -4,18 +4,18 @@ do
 	OTISAK=$(openssl passwd -apr1 -salt lozinka1 lozinka$i)
 	if [[ "$OTISAK" =~ "NayNtGqjZ5FzvSulsQvaa." ]]
 	then
-		echo "lozinka$i + -apr1 algo + salt lozinka1 -> NayNtGqjZ5FzvSulsQvaa."
+		echo "lozinka$i + apr1 algo + salt lozinka1 -> NayNtGqjZ5FzvSulsQvaa."
 	fi
 
 	OTISAK=$(openssl passwd -apr1 -salt lozinka7 lozinka$i)
 	if [[ "$OTISAK" =~ "EneC7HhVrfMd5p6KWT8VT." ]]
 	then
-		echo "lozinka$i + -apr1 algo + salt lozinka7 -> EneC7HhVrfMd5p6KWT8VT."
+		echo "lozinka$i + apr1 algo + salt lozinka7 -> EneC7HhVrfMd5p6KWT8VT."
 	fi
 
 	OTISAK=$(openssl passwd -apr1 -salt wAgnh5WA lozinka$i)
 	if [[ "$OTISAK" =~ "CyPYmkLRTzbSN1fIQROnu1" ]]
 	then
-		echo "lozinka$i + -apr1 algo + salt wAgnh5WA -> CyPYmkLRTzbSN1fIQROnu1"
+		echo "lozinka$i + apr1 algo + salt wAgnh5WA -> CyPYmkLRTzbSN1fIQROnu1"
 	fi
 done
--- ispit_20201007/z02/solution.sh
+++ ispit_20201007/z02/solution.sh
@@ -13,13 +13,13 @@ for i in {1..3}; do mv otisci/otisak$i.txt otisci/otisak$i.base64; done
 openssl enc -ciphers | sed "1d" | tr -s " " "\n" | cut -c2- | grep-E "^aes*" > aes.algos
 #Ovo nije proslo zbog verzije, pa sam rucno moramo da ih iz help-a kopiram.
 
-#Napisimo skriput za pronalazenje otisaka.
+#Napisimo skriptu za pronalazenje otisaka.
 touch script-otisci.sh && chmod 777 script-otisci.sh
 ./script-otisci.sh 
 #Izlaz je:
-#lozinka3 + -apr1 algo + salt lozinka7 -> EneC7HhVrfMd5p6KWT8VT.
-#lozinka18 + -apr1 algo + salt lozinka1 -> NayNtGqjZ5FzvSulsQvaa.
-#lozinka30 + -apr1 algo + salt wAgnh5WA -> CyPYmkLRTzbSN1fIQROnu1
+#lozinka3 + apr1 algo + salt lozinka7 -> EneC7HhVrfMd5p6KWT8VT.
+#lozinka18 + apr1 algo + salt lozinka1 -> NayNtGqjZ5FzvSulsQvaa.
+#lozinka30 + apr1 algo + salt wAgnh5WA -> CyPYmkLRTzbSN1fIQROnu1
 
 #Koristimo 3, 18 i 30 lozinke za dekrpiciju. Napisimo skriptu za to.
 touch script.sh && chmod 777 script.sh
@@ -30,4 +30,4 @@ touch script.sh && chmod 777 script.sh
 ./script.sh lozinka30 lozinka3 lozinka18 2>/dev/null #Rjesenje!
 ./script.sh lozinka30 lozinka18 lozinka30 2>/dev/null
 #Dobijemo ispis: "aes-192-cfb + lozinka30-lozinka3-lozinka18 -> Idemo dalje :)!"
-#Kljucevi su u redoslijedu njihoog koristenja za dekripciju lozinka30, lozinka3, lozinka18, koristenjem aes-192-cfb algoritma dobija se smilsen sadrzaj "Idemo dalje :)!"
+#Kljucevi su u redoslijedu njihovog koristenja za dekripciju lozinka30, lozinka3, lozinka18, koristenjem aes-192-cfb algoritma dobija se smilsen sadrzaj "Idemo dalje :)!"
