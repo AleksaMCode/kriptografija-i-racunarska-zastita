6aaabf8a5021fc0f1d921e7beb26a08efad2d5c7
Aleksa Majkic <aleksamcode@gmail.com>
2021-02-01 13:11:59 +0100

    Script updated.

--- ispit_20200701/z01/script.sh
+++ ispit_20200701/z01/script.sh
@@ -2,12 +2,8 @@
 while IFS= read -r algo
 do
     for i in {1..20}
-    do
-        openssl dgst -$algo -out ulaz-otisak.txt ulazi/ulaz$i.txt
-        VAR1=$(cat ulaz-otisak.txt)
-        VAR2=$(cat otisak.txt)
-        rm ulaz-otisak.txt
-        if [[ "$VAR1" =~ "$VAR2" ]]
+    do             
+        if [[ "$(openssl dgst -$algo ulazi/ulaz$i.txt)" =~ "$(cat otisak.txt)" ]]
         then
             echo -n "Solution is ulaz$i.txt ($algo) file with pa Myszkowski password '" && cat ulazi/ulaz$i.txt && echo "'"
             break 2
